e8292ec2f803307d11fbc0a14033680f8a88a780
silenceofranger <38451055+silenceofranger@users.noreply.github.com>
2020-11-07 01:48:35 +0530

    tyrt

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="main">
+        <ol>
+          <li key="relativeListItem1"> ramesh</li>
+          <li key="relativeListItem2"> amesh</li>
+          <li key="relativeListItem3"> fghramesh</li>
+          <li key="relativeListItem4"> framesh</li>
+          <li key="relativeListItem5"> ddramesh</li>
+        </ol>
+      </div>
+    );
+  }
 }
-
 
 export default App;
